96ee56c8e6e847b4cf7373015c317cb9f323aa3d
pallavikabadi <58931162+pallavikabadi@users.noreply.github.com>
2023-05-02 14:08:02 +0530

    Added:Employee Wage Using Class Method And Class Variable

--- src/com/bridgelabz/EmpWedge.java
+++ src/com/bridgelabz/EmpWedge.java
@@ -3,13 +3,10 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmpWedge {
-    
+
     private static final int wagePerHour = 20;
     private static final int workingDays = 20;
     private static final int max_workingHours = 100;
-
-    public static void main(String[] args) {
-        System.out.println("*** Welcome To Employee Wage Computation Program ***");
 
         /*//UC1
         Random random = new Random();
@@ -65,42 +62,49 @@ public class EmpWedge {
         dailyWage = workingHour * wagePerHour;
         System.out.println("Employee Daily Wage = " +dailyWage);
     }*/
+    //UC7:
+    public void EmployeeWage() {
 
+        int totalWage = 0;
+        int dailyWage;
+        int workingHour = 0;
+        int totalWorkingHour = 0;
+        int day = 1;
 
-
-            int totalWage = 0;
-            int dailyWage;
-            int workingHour = 0;
-            int totalWorkingHour = 0;
-            int day = 1;
-
-            while (day <= workingDays && workingHour <= max_workingHours) {
-                Random random = new Random();
-                int present = random.nextInt(3);
-                System.out.println();
-                System.out.println("DAY => " + day + ";");
-                switch (present) {
-                    case 0:
-                        System.out.println("Employee is ABSENT");
-                        workingHour = 0;
-                        break;
-                    case 1:
-                        System.out.println("Employee is PRESENT");
-                        workingHour = 8;
-                        break;
-                    default:
-                        System.out.println("Employee is PART-TIME");
-                        workingHour = 4;
-                        break;
-                }
-                day++;
-                dailyWage = workingHour * wagePerHour;
-                totalWage += dailyWage;
-                totalWorkingHour += workingHour;
-                System.out.println("Employee Working Hour Per Day = " + workingHour);
-                System.out.println("Employee Daily Wage = " + dailyWage);
+        while (day <= workingDays && workingHour <= max_workingHours) {
+            Random random = new Random();
+            int present = random.nextInt(3);
+            System.out.println();
+            System.out.println("DAY => " + day + ";");
+            switch (present) {
+                case 0:
+                    System.out.println("Employee is ABSENT");
+                    workingHour = 0;
+                    break;
+                case 1:
+                    System.out.println("Employee is PRESENT");
+                    workingHour = 8;
+                    break;
+                default:
+                    System.out.println("Employee is PART-TIME");
+                    workingHour = 4;
+                    break;
             }
-            System.out.println("Employee Monthly Wage = " + totalWage + " $ ");
-
+            day++;
+            dailyWage = workingHour * wagePerHour;
+            totalWage += dailyWage;
+            totalWorkingHour += workingHour;
+            System.out.println("Employee Working Hour Per Day = " + workingHour);
+            System.out.println("Employee Daily Wage = " + dailyWage);
         }
+        System.out.println("Employee Monthly Wage = " + totalWage + " $ ");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program ");
+
+        EmpWedge empWedge=new EmpWedge();
+        empWedge.EmployeeWage();
+    }
 }
