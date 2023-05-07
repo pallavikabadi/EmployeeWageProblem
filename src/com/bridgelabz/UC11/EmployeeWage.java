f63193f9f9c58f644d459420df7cc970bbc3b259
pallavikabadi <58931162+pallavikabadi@users.noreply.github.com>
2023-05-07 23:30:50 +0530

    Added:Total Wage When Queried By Company

--- src/com/bridgelabz/UC11/EmployeeWage.java
+++ src/com/bridgelabz/UC11/EmployeeWage.java
@@ -2,5 +2,7 @@ package com.bridgelabz.UC11;
 
 public interface EmployeeWage {
     int ComputeEmpWage();
+    public void addCompanyEmpWage(String companyName, int wagePerHour, int workingDays, int max_workingHours);
+    public int getTotalWage(String company);
 
 }
