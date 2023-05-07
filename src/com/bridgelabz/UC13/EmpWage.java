006382d429605151322ff8ec44cc6aa9c0ef18ed
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-05-07 22:45:08 +0530

    Added:Store the Daily

--- src/com/bridgelabz/UC13/EmpWage.java
+++ src/com/bridgelabz/UC13/EmpWage.java
@@ -1,4 +1,5 @@
-package com.bridgelabz.UC12;
+
+package com.bridgelabz.UC13;
 
 import java.util.ArrayList;
 
@@ -13,17 +14,17 @@ public class EmpWage {
 
         companyEmpWages.add(new CompanyEmpWage("TCS",30,24,121));
         int empWageTcs = companyEmpWages.get(0).ComputeEmpWage();
-        System.out.println(companyEmpWages.get(0));
+        //System.out.println(companyEmpWages.get(0));
         System.out.println("Employee of Tcs Monthly Wage is " + empWageTcs + " $ ");
         System.out.println();
         companyEmpWages.add(new CompanyEmpWage("Infosys",20,22,130));
         int empWageInfosys = companyEmpWages.get(1).ComputeEmpWage();
-        System.out.println(companyEmpWages.get(1));
+        //System.out.println(companyEmpWages.get(1));
         System.out.println("Employee of Infosys Monthly Wage is " + empWageInfosys + " $ ");
         System.out.println();
         companyEmpWages.add(new CompanyEmpWage("Mindtree",50,22,160));
         int empWageMindtree = companyEmpWages.get(2).ComputeEmpWage();
-        System.out.println(companyEmpWages.get(2));
+        //System.out.println(companyEmpWages.get(2));
         System.out.println("Employee of Middtree Wage is " + empWageMindtree+ " $ ");
     }
 
---
+++ src/com/bridgelabz/UC13/EmployeeWage.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.UC13;
+
+public interface EmployeeWage {
+    int ComputeEmpWage();
+
+}
